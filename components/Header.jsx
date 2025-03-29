f1958a0b4a88099094f886135285962a466a5a5d
sukanya gurav <sukanyagurav6@gmail.com>
2025-03-29 20:32:09 +0530

    updated header

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,24 +5,19 @@ import { useEffect, useState } from 'react';
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null; // Avoid rendering until theme is available
 
   return (
     <header className="max-w-[1200px] mx-auto p-8 px-4 w-full flex justify-between items-center gap-4">
-      <Image
-        src={`/images/logo-${theme === 'dark' ? 'dark' : 'light'}-theme.svg`}
-        alt="Character Count logo"
-        width="240"
-        height="240"
-        priority
-        unoptimized
-      />
+      {theme && (
+        <Image
+          src={`/images/logo-${theme === 'dark' ? 'dark' : 'light'}-theme.svg`}
+          alt="Character Count logo"
+          width={240}
+          height={240}
+          priority
+          unoptimized
+        />
+      )}
       <button
         onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
         className="p-2 bg-gray-200 dark:bg-[#2b2b37] rounded-md"
